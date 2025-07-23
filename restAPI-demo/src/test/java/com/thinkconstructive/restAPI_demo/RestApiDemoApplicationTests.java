a31f73281a6f44ba12441ca16c99c7ef9bdc6348
Keerthi2040 <keerthimaiakurathi20@gmail.com>
2025-07-23 11:08:04 -0500

    Initial commit - Cloud Vendor Spring Boot REST API

---
+++ restAPI-demo/src/test/java/com/thinkconstructive/restAPI_demo/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkconstructive.restAPI_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
